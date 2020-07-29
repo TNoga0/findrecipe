1a47f74b07366a996494fe5d54bf514aed8c750d
TNoga0 <t.noga0@gmail.com>
2020-07-29 22:19:21 +0200

    try true debug

--- fridge_reciper/settings.py
+++ fridge_reciper/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ["localhost", "192.168.0.31"]
 ALLOWED_HOSTS = [".herokuapp.com"]
