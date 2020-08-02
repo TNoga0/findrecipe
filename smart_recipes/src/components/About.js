3fe21a9fab489a2bdb9cb36b84591b76d0045215
TNoga0 <t.noga0@gmail.com>
2020-08-02 20:16:06 +0200

    get rid of footer and update title

--- smart_recipes/src/components/About.js
+++ smart_recipes/src/components/About.js
@@ -10,7 +10,9 @@ function About() {
         This app was made with a simple purpose: to be useful.
       </p>
       <p className="content">
-        It uses Django with Gunicorn and React.<br/>
+        It uses Django with Gunicorn and React.<br/> All the data is served from database, which is populated by
+        scraping AllRecipes. <br/> The scraping process is obviously imperfect and some ingredients may be followed by non-typical
+        words or abbreviations.
       </p>
       <p className="content">
         Feel free to visit my <a href="https://github.com/TNoga0">GitHub</a>, although
